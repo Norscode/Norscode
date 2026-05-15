2ef2d66cd3d4f840be1b40db22ac5963b0d939d0
rfwwp8k542-maker <jan.s.saetre@icloud.com>
2026-05-15 21:09:03 +0200

    Register selfhost lexer status command

--- norcode/commands/registry.py
+++ norcode/commands/registry.py
@@ -22,6 +22,7 @@ from norcode.commands.lexer_parity_check import LEXER_PARITY_CHECK_COMMAND
 from norcode.commands.lexer_parity_fixture import LEXER_PARITY_FIXTURE_COMMAND
 from norcode.commands.parser_parity_fixture import PARSER_PARITY_FIXTURE_COMMAND
 from norcode.commands.run import RUN_COMMAND
+from norcode.commands.selfhost_lexer_status import SELFHOST_LEXER_STATUS_COMMAND
 from norcode.commands.test import TEST_COMMAND
 
 
@@ -36,6 +37,7 @@ COMMANDS: tuple[CommandModule, ...] = (
     PARSER_PARITY_FIXTURE_COMMAND,
     LEXER_PARITY_FIXTURE_COMMAND,
     LEXER_PARITY_CHECK_COMMAND,
+    SELFHOST_LEXER_STATUS_COMMAND,
     CommandModule(
         name="repl",
         help="Start en enkel interaktiv Norscode-REPL",
