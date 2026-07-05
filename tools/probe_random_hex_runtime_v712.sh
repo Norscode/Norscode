869264533c130cbdd8b6e169fc929f07adc21b97
Jan Steinar Sætre <jan.s.saetre@gmail.com>
2026-07-05 10:55:32 +0200

    Move random runtime probes into Norscode

---
+++ tools/probe_random_hex_runtime_v712.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# Tynn wrapper: v712-proben ligg i .no-fila.
+set -eu
+
+ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
+cd "$ROOT"
+
+export NORSCODE_ENABLE_EXEC_PROSESS="${NORSCODE_ENABLE_EXEC_PROSESS:-1}"
+exec "$ROOT/bin/nc" run "$ROOT/tools/probe_random_hex_runtime_v712.no"
